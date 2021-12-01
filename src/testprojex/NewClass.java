53c83053a3e53d660db9e483b31de4ffa814d8b5
bentontran <bentontran05@gmail.com>
2021-12-01 08:39:38 -0800

    Initial commit

---
+++ src/testprojex/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojex;
+
+/**
+ *
+ * @author 143001811
+ */
+public class NewClass {
+    //code for new class
+}
---
+++ src/testprojex/TestProjEx.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojex;
+
+/**
+ *
+ * @author 143001811
+ */
+public class TestProjEx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //code
+        System.out.println("HI!");
+        //hi code
+    }
+    
+}
